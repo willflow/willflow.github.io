228d7a7fdf9454bf34f67c87c36b56ae5e91a935
zhangm <willflowuu@gmail.com>
2018-07-12 21:52:11 +0800

    pull

--- miao/lodash/willflow-lodash.js
+++ miao/lodash/willflow-lodash.js
@@ -63,7 +63,7 @@ var willflow = {
   fill: function(array,value,start,end){
     var start = start || 0
     var end = array.length
-    for(let i = start; i < end; i++){
+    for(let i = start; i <= end; i++){
       array[i] = value
     }
     return array
@@ -85,7 +85,60 @@ var willflow = {
     array.length = len - 1
     return array
   },
-  lasr: function(array){
+  last: function(array){
     return array[array.length - 1]
-  }
+  },
+  lastIndexOf: function(array,value,fromindex){
+    var fromindex = fromindex || 0
+    array.reverse()
+    for(let i = fromindex; i < array.length; i++){
+      if(array[i] == value){
+        return i
+      }
+    }
+    return -1
+  },
+  nth: function(array,n){
+    var n = n || 0
+    if(n >= 0){
+      return array[n]
+    }else{
+      array.reverse()
+      return array[n]
+    }
+  },
+  pull: function(array,value){
+    let len = arguments.length
+    let ary = []
+    for (let i = 1; i < len; i++) {
+      ary = ary.concat(arguments[i])
+    }
+    for(let i = 0; i < array.length; i++){
+      for(let j = 0; j > ary.length; j++){
+        if(array[i] == ary[j]){
+          array.splice(i,1)
+          i--
+          break
+        }
+      }
+    }
+    return array
+  },
+  pullAll: function(array,value){
+    let len = arguments.length
+    let ary = []
+    for (let i = 1; i < len; i++) {
+      ary = ary.concat(arguments[i])
+    }
+    for(let i = 0; i < array.length; i++){
+      for(let j = 0; j > ary.length; j++){
+        if(array[i] == ary[j]){
+          array.splice(i,1)
+          i--
+          break
+        }
+      }
+    }
+    return array
+  },
 }
