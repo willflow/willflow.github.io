5503ba1a3942f921e44fd2b43541025c35109a27
zhangm <willflowuu@gmail.com>
2018-07-12 15:07:42 +0800

    3

--- miao/lodash/willflow-lodash.js
+++ miao/lodash/willflow-lodash.js
@@ -1,25 +1,43 @@
 var willflow = {
-  chunk: function(array,size){
+  chunk: function (array, size) {
     let len = array.length
     let ary1 = []
     let ary2 = []
     let result = []
-    for(let i = 0; i < size; i++){
+    for (let i = 0; i < size; i++) {
       ary1.push(array[i])
     }
-    for(let i = size; i < len; i++){
+    for (let i = size; i < len; i++) {
       ary2.push(array[i])
     }
     result.push(ary1)
     result.push(ary2)
     return result
   },
-  compact: function(array){
+  compact: function (array) {
     let len = array.length
     let result = []
-    for(let i = 0; i < len; i++){
-      if(Boolean(array[i]) == true) result.push(array[i])
+    for (let i = 0; i < len; i++) {
+      if (Boolean(array[i]) == true) result.push(array[i])
     }
+    return result
+  },
+  difference: function (array, values) {
+    let len = arguments.length
+    let result = []
+    let ary = []
+    for (let i = 1; i < len; i++) {
+      ary = ary.concat(arguments[i])
+    }
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < ary.length; j++) {
+        if (array[i] == ary[j]) {
+          array.splice(i, 1)
+          i--
+        }
+      }
+    }
+    result = array
     return result
   }
 }
