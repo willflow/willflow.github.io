63e94b678e1f978873c575719983b5df42340c71
zhangm <willflowuu@gmail.com>
2018-07-12 11:44:57 +0800

    lodash 1

--- miao/lodash/willflow-lodash.js
+++ miao/lodash/willflow-lodash.js
@@ -1,3 +1,17 @@
 var willflow = {
-  
+  chunk: function(array,size){
+    let len = array.length
+    let ary1 = []
+    let ary2 = []
+    let result = []
+    for(let i = 0; i < size; i++){
+      ary1.push(array[i])
+    }
+    for(let i = size; i < len; i++){
+      ary2.push(array[i])
+    }
+    result.push(ary1)
+    result.push(ary2)
+    return result
+  }
 }
